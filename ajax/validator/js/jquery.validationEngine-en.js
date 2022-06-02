28423ca9f4754fa704cc302237f8bf80afa90ed5
Alex Bengo <alex.bengo@gmail.com>
2022-06-02 14:57:20 +0300

    Add files via upload
    Added CSS files, file upload class and Ajax files

---
+++ ajax/validator/js/jquery.validationEngine-en.js
@@ -0,0 +1,70 @@
+
+
+(function($) {
+	$.fn.validationEngineLanguage = function() {};
+	$.validationEngineLanguage = {
+		newLang: function() {
+			$.validationEngineLanguage.allRules = 	{"required":{    			// Add your regex rules here, you can take telephone as an example
+						"regex":"none",
+						"alertText":"* This field is required",
+						"alertTextCheckboxMultiple":"* Please select an option",
+						"alertTextCheckboxe":"* This checkbox is required"},
+					"length":{
+						"regex":"none",
+						"alertText":"*Between ",
+						"alertText2":" and ",
+						"alertText3": " characters allowed"},
+					"maxCheckbox":{
+						"regex":"none",
+						"alertText":"* Checks allowed Exceeded"},	
+					"minCheckbox":{
+						"regex":"none",
+						"alertText":"* Please select ",
+						"alertText2":" options"},	
+					"confirm":{
+						"regex":"none",
+						"alertText":"* Your field is not matching"},		
+					"phone":{
+						"regex":"/^[0-9\-\(\)\ ]+$/",
+						"alertText":"* Invalid phone number"},	
+					"email":{
+						"regex":"/^[a-zA-Z0-9_\.\-]+\@([a-zA-Z0-9\-]+\.)+[a-zA-Z0-9]{2,4}$/",
+						"alertText":"* Invalid email address"},	
+					"date":{
+                         "regex":"/^[0-9]{4}\-\[0-9]{1,2}\-\[0-9]{1,2}$/",
+                         "alertText":"* Invalid date, must be in YYYY-MM-DD format"},
+					"onlyNumber":{
+						"regex":"/^[0-9\ ]+$/",
+						"alertText":"* Numbers only"},	
+					"noSpecialCaracters":{
+						"regex":"/^[0-9a-zA-Z]+$/",
+						"alertText":"* No special caracters allowed"},	
+					"ajaxUser":{
+						"file":"validateUser.php",
+						"extraData":"name=eric",
+						"alertTextOk":"* This Email is available",	
+						"alertTextLoad":"* Loading, please wait",
+						"alertText":"* This Email is already taken"},	
+					"ajaxName":{
+						"file":"validateCompany.php",
+						"alertText":"* Company Exists in the Directory",
+						"alertTextOk":"* Proceed With Registration",	
+						"alertTextLoad":"* Loading, please wait"},		
+					"onlyLetter":{
+						"regex":"/^[a-zA-Z\ \']+$/",
+						"alertText":"* Letters only"},
+					"validate2fields":{
+    					"nname":"validate2fields",
+    					"alertText":"* You must have a firstname and a lastname"},	
+					"validate4fields":{
+    					"nname":"validate4fields",
+    					"alertText":"* Your passwords should match"}	
+				}	
+					
+		}
+	}
+})(jQuery);
+
+$(document).ready(function() {	
+	$.validationEngineLanguage.newLang()
+});
